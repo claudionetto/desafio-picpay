5416b8aa0ee3c7950a0dc5a47ea04c3a48bb7d34
Claudio Netto Junior <claudio.netto@sou.unifeob.edu.br>
2024-02-27 12:05:56 -0300

    refactor: improving notification service and rename method

--- src/main/java/com/claudionetto/desafiopicpay/services/NotificationService.java
+++ src/main/java/com/claudionetto/desafiopicpay/services/NotificationService.java
@@ -1,30 +1,38 @@
 package com.claudionetto.desafiopicpay.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class NotificationService {
 
     private final RestTemplate restTemplate;
 
-    public boolean sendNotifation(String email) {
+    public boolean sendNotification(String email) {
         String url = "https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6";
 
-        Map noticationSent = restTemplate.getForObject(url, Map.class);
+        try {
+            Map<String, Object> notificationSent = restTemplate.getForObject(url, Map.class);
 
-        boolean isNotificationSent = Boolean.getBoolean(noticationSent.get("message").toString());
+            if (notificationSent != null && notificationSent.get("message") != null) {
+                boolean isNotificationSent = Boolean.parseBoolean(notificationSent.get("message").toString());
 
-        if (isNotificationSent) {
-            System.out.println("Email enviado para: " + email);
-            return true;
+                if (isNotificationSent) {
+                    log.info("Email enviado para: " + email);
+                    return true;
+                }
+            }
+        } catch (RestClientException e) {
+            log.error("Erro ao chamar o serviço de notificação: " + e.getMessage());
         }
 
         return false;
     }
-
 }
