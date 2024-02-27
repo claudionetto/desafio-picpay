cbe95bcf1988eeb5738afe61d2e559686f6f0889
Claudio Netto Junior <claudio.netto@sou.unifeob.edu.br>
2024-02-27 11:31:13 -0300

    test: improving tests for Transaction Service

--- src/test/java/com/claudionetto/desafiopicpay/services/TransactionServiceTest.java
+++ src/test/java/com/claudionetto/desafiopicpay/services/TransactionServiceTest.java
@@ -8,7 +8,7 @@ import com.claudionetto.desafiopicpay.dto.TransactionResponseDTO;
 import com.claudionetto.desafiopicpay.dto.UserResponseDTO;
 import com.claudionetto.desafiopicpay.exceptions.UnauthorizedTransactionException;
 import com.claudionetto.desafiopicpay.repositories.TransactionRepository;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -38,34 +38,38 @@ class TransactionServiceTest {
     @InjectMocks
     private TransactionService transactionService;
 
+    private User payer;
+    private User payee;
+    private TransactionDTO transactionDTO;
+
+    @BeforeEach
+    void setUp(){
+        this.payer = new User(1L, "Claudio", "Netto", "claudio@gmail.com",
+                "123123123", "12345678", UserType.COMMON, BigDecimal.valueOf(50));
+        this.payee = new User(2L, "José", "Netto", "jose@gmail.com",
+                "321321321", "12345678", UserType.MERCHANT, BigDecimal.valueOf(50));
+        this.transactionDTO = new TransactionDTO(BigDecimal.valueOf(20), 1L, 2L);
+    }
+
     @Test
     @DisplayName("Should create transaction successfully when everything is OK")
     void createTransaction_ValidTransaction_ShouldReturnResponseDTO_WhenSuccessful() {
 
-
-        User payer = new User(1L, "Claudio", "Netto", "claudio@gmail.com",
-                "123123123", "12345678", UserType.COMMON, BigDecimal.valueOf(50));
-        User payee = new User(2L, "José", "Netto", "jose@gmail.com",
-                "321321321", "12345678", UserType.MERCHANT, BigDecimal.valueOf(50));
-
-        TransactionDTO transactionDTO = new TransactionDTO(new BigDecimal(20), 1L, 2L);
-
-        when(userService.findById(1L)).thenReturn(payer);
-        when(userService.findById(2L)).thenReturn(payee);
-
+        when(userService.findById(1L)).thenReturn(this.payer);
+        when(userService.findById(2L)).thenReturn(this.payee);
         when(authorizationService.authorizeTransaction()).thenReturn(true);
 
-        UserResponseDTO userResponsePayer = new UserResponseDTO("Claudio", "Netto", "claudio@gmail.com",
-                "12345678", UserType.COMMON, BigDecimal.valueOf(50));
-        UserResponseDTO userResponsePayee = new UserResponseDTO("José", "Netto", "jose@gmail.com",
-                "321321321", UserType.MERCHANT, BigDecimal.valueOf(50));
+        UserResponseDTO userResponsePayer = new UserResponseDTO("Claudio", "Netto",
+                "claudio@gmail.com", "12345678", UserType.COMMON, BigDecimal.valueOf(50));
+        UserResponseDTO userResponsePayee = new UserResponseDTO("José", "Netto",
+                "jose@gmail.com", "321321321", UserType.MERCHANT, BigDecimal.valueOf(50));
         TransactionResponseDTO transactionResponseDTOMock = new TransactionResponseDTO(1L, userResponsePayee,
                 userResponsePayer, new BigDecimal(50), LocalDateTime.now());
 
         when(transactionConverter.toTransactionResponseDTO(any()))
                 .thenReturn(transactionResponseDTOMock);
 
-        TransactionResponseDTO responseDTO = transactionService.createTransaction(transactionDTO);
+        TransactionResponseDTO responseDTO = transactionService.createTransaction(this.transactionDTO);
 
         assertNotNull(responseDTO);
         assertEquals(responseDTO ,transactionResponseDTOMock);
@@ -81,48 +85,29 @@ class TransactionServiceTest {
     @DisplayName("Should throw a UnauthorizedTransactionException when transaction is unauthorized")
     void createTransaction_ShouldThrowUnauthorizedTransactionException_WhenTransactionIsUnauthorized() {
 
-
-        User payer = new User(1L, "Claudio", "Netto", "claudio@gmail.com",
-                "123123123", "12345678", UserType.COMMON, BigDecimal.valueOf(50));
-        User payee = new User(2L, "José", "Netto", "jose@gmail.com",
-                "321321321", "12345678", UserType.MERCHANT, BigDecimal.valueOf(50));
-
-
-        when(userService.findById(1L)).thenReturn(payer);
-        when(userService.findById(2L)).thenReturn(payee);
-
+        when(userService.findById(1L)).thenReturn(this.payer);
+        when(userService.findById(2L)).thenReturn(this.payee);
         when(authorizationService.authorizeTransaction()).thenReturn(false);
 
-        TransactionDTO transactionDTO = new TransactionDTO(new BigDecimal(20), 1L, 2L);
-
         UnauthorizedTransactionException exception = assertThrows(UnauthorizedTransactionException.class,
-                () -> transactionService.createTransaction(transactionDTO)
+                () -> transactionService.createTransaction(this.transactionDTO)
         );
 
         assertEquals("Transação não autorizada", exception.getMessage());
-
         verify(transactionRepository, times(0)).save(any());
-
     }
 
     @Test
     @DisplayName("Shouldn't send notification when there is a exception")
     void createTransaction_ShouldNotSendNotification_WhenThereIsException() {
 
-        User payer = new User(1L, "Claudio", "Netto", "claudio@gmail.com",
-                "123123123", "12345678", UserType.COMMON, BigDecimal.valueOf(50));
-        User payee = new User(2L, "José", "Netto", "jose@gmail.com",
-                "321321321", "12345678", UserType.MERCHANT, BigDecimal.valueOf(50));
-
-        when(userService.findById(1L)).thenReturn(payer);
-        when(userService.findById(2L)).thenReturn(payee);
+        when(userService.findById(1L)).thenReturn(this.payer);
+        when(userService.findById(2L)).thenReturn(this.payee);
         when(authorizationService.authorizeTransaction()).thenReturn(true);
 
         doThrow(new RuntimeException("Simulate exception in updateBalance")).when(userService).updateBalance(any());
 
-        TransactionDTO transactionDTO = new TransactionDTO(new BigDecimal(20), 1L, 2L);
-
-        assertThrows(RuntimeException.class, () -> transactionService.createTransaction(transactionDTO));
+        assertThrows(RuntimeException.class, () -> transactionService.createTransaction(this.transactionDTO));
 
         verifyNoInteractions(notificationService);
     }
